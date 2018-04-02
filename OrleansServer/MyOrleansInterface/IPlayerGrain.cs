2778908de26c27bd714dae86005b0cbc663dae6c
polei <695166116@qq.com>
2018-04-02 14:30:51 +0800

    Orleans update to 2.0

---
+++ OrleansServer/MyOrleansInterface/IPlayerGrain.cs
@@ -0,0 +1,17 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOrleansInterface
+{
+    //an example of a Grain Interface
+    public interface IPlayerGrain : IGrainWithGuidKey
+    {
+        Task<IGameGrain> GetCurrentGame();
+        Task JoinGame(IGameGrain game);
+        Task LeaveGame(IGameGrain game);
+    }
+}
